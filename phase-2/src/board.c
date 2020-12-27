cf61f915fdc1781a7f28412092f88a8635513a49
myimah <esteban.morisse14@gmail.com>
2020-12-27 17:21:55 +0100

    final phase-2

--- phase-2/src/board.c
+++ phase-2/src/board.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
-
 #include "board.h"
 
 /**
- * \file board.c
+ *\file board.c
  *
- * \brief Source code associated with \ref board.h
+ *\brief Source code associated with \ref board.h
  *
- * \author Guibout & Morisse
+ *\author Guibout &Morisse
  */
 
 /**
- * @brief The board of the game, define it as you wish.
+ *@brief The board of the game, define it as you wish.
  */
 
 struct board_s
@@ -47,6 +46,8 @@ void clear_game(board game)
 			game->grid[i][j] = NONE;
 		}
 	}
+
+	// Reseting all variables to default values
 	game->picked_owner = NO_PLAYER;
 	game->picked_column = -1;
 	game->picked_line = -1;
@@ -80,6 +81,46 @@ board new_game()
 	return new_board;
 }
 
+board copy_game(board game)
+{
+	board copy = new_game(game);
+
+	for (int i = 0; i < DIMENSION - 1; i++)
+	{
+		for (int j = 0; j < DIMENSION - 1; j++)
+		{
+			copy->grid[i][j] = game->grid[i][j];
+		}
+	}
+
+	// Copying all variables from the original board to the copy
+	copy->picked_owner = game->picked_owner;
+	copy->picked_column = game->picked_column;
+	copy->picked_line = game->picked_line;
+	copy->picked_size = game->picked_size;
+
+	copy->south_goal_occupied = game->south_goal_occupied;
+	copy->north_goal_occupied = game->north_goal_occupied;
+
+	copy->movement_left = game->movement_left;
+
+	for (int i = 0; i < 3; i++)
+	{
+		copy->moves[i] = game->moves[i];
+	}
+
+	copy->origin_line = game->origin_line;
+	copy->origin_column = game->origin_column;
+
+	for (int i = 0; i < NB_SIZE; i++)
+	{
+		copy->pieces_left[SOUTH_P - 1][i] = game->pieces_left[SOUTH_P - 1][i];
+		copy->pieces_left[NORTH_P - 1][i] = game->pieces_left[NORTH_P - 1][i];
+	}
+
+	return copy;
+}
+
 void destroy_game(board game)
 {
 	// freeing memory, necessary for memory allocated with malloc (leave as is)
@@ -92,6 +133,7 @@ bool in_board(int line, int column)
 	{
 		if (0 <= column && column <= DIMENSION - 1)
 		{
+			// If in board, return true
 			return true;
 		}
 	}
@@ -106,6 +148,7 @@ size get_piece_size(board game, int line, int column)
 		return NONE;
 	}
 
+	// Return the piece's size if it's in board
 	return game->grid[line][column];
 }
 
@@ -120,6 +163,7 @@ player get_winner(board game)
 		return NORTH_P;
 	}
 
+	// Returning no player if there is no winner this turn
 	return NO_PLAYER;
 }
 
@@ -150,6 +194,7 @@ int northmost_occupied_line(board game)
 			}
 		}
 	}
+
 	return -1;
 }
 
@@ -184,6 +229,7 @@ int nb_pieces_available(board game, size piece, player player)
 	{
 		return -1;
 	}
+
 	return game->pieces_left[player - 1][piece - 1];
 }
 
@@ -204,14 +250,17 @@ return_code place_piece(board game, size piece, player current_player, int colum
 		return PARAM;
 	}
 
-	if ((piece < 1 || piece > NB_SIZE) || !in_board(line, column))
+	// If the piece size is not a valid size or the coordinates are not correct return PARAM code
+	if (!(0 < piece && piece <= NB_SIZE) || !in_board(line, column))
 	{
 		return PARAM;
 	}
+	// if the position is not empty return EMPTY code
 	else if (get_piece_size(game, line, column) != NONE)
 	{
 		return EMPTY;
 	}
+	// if the playe has not enough pieces left
 	else if (nb_pieces_available(game, piece, current_player) == 0)
 	{
 		return FORBIDDEN;
@@ -227,16 +276,19 @@ return_code pick_piece(board game, player current_player, int line, int column)
 {
 	size piece_size = get_piece_size(game, line, column);
 
+	// If it's not in board or no player is currently playing, then its a PARAM error
 	if (!in_board(line, column) || current_player == NO_PLAYER)
 	{
 		return PARAM;
 	}
 
+	// If there is no piece inside the cell, return EMPTY code 
 	if (piece_size == NONE)
 	{
 		return EMPTY;
 	}
 
+	// Checking if line is the correct one according to the current player 
 	if (current_player == SOUTH_P)
 	{
 		if (southmost_occupied_line(game) != line)
@@ -252,11 +304,13 @@ return_code pick_piece(board game, player current_player, int line, int column)
 		}
 	}
 
+	// If there is a winner, return FORBIDDEN code
 	if (get_winner(game) != NO_PLAYER)
 	{
 		return FORBIDDEN;
 	}
 
+	// Picking the piece
 	game->picked_line = line;
 	game->picked_column = column;
 	game->picked_size = piece_size;
@@ -265,7 +319,7 @@ return_code pick_piece(board game, player current_player, int line, int column)
 	game->origin_column = column;
 	game->movement_left = piece_size;
 	game->grid[line][column] = NONE;
-	
+
 	return OK;
 }
 
@@ -279,32 +333,40 @@ player next_player(player current_player)
 	return SOUTH_P;
 }
 
+/**
+ *@brief A function that returns the movement of a piece according to a direction
+ * 
+ *@param dir the direction
+ * 
+ *@returns the pointer to an array of 2 numbers : {y, x}. If the direction is GOAL or not valid the function will return {0, 0}
+ **/
 int *move(direction dir)
 {
 	static int movement[2];
 
+	// Acts like in a dual axis plan
 	switch (dir)
 	{
-	case NORTH:
-		movement[0] = 1;
-		movement[1] = 0;
-		break;
-	case SOUTH:
-		movement[0] = -1;
-		movement[1] = 0;
-		break;
-	case EAST:
-		movement[0] = 0;
-		movement[1] = 1;
-		break;
-	case WEST:
-		movement[0] = 0;
-		movement[1] = -1;
-		break;
-	default:
-		movement[0] = 0;
-		movement[1] = 0;
-		break;
+		case NORTH:
+			movement[0] = 1;
+			movement[1] = 0;
+			break;
+		case SOUTH:
+			movement[0] = -1;
+			movement[1] = 0;
+			break;
+		case EAST:
+			movement[0] = 0;
+			movement[1] = 1;
+			break;
+		case WEST:
+			movement[0] = 0;
+			movement[1] = -1;
+			break;
+		default:
+			movement[0] = 0;
+			movement[1] = 0;
+			break;
 	}
 
 	return movement;
@@ -319,15 +381,19 @@ bool is_move_possible(board game, direction direction)
 
 	if (direction == GOAL)
 	{
+		// If the north player is not trying to go in his own goal and he can move...
 		if (picked_piece_owner(game) == NORTH_P && line == 0 && movement_left(game) == 1)
 		{
 			return true;
 		}
+		// Also checking for the south player
 		else if (picked_piece_owner(game) == SOUTH_P && line == 5 && movement_left(game) == 1)
 		{
 			return true;
 		}
 	}
+
+	// If the player is not trying to do a winning move, it's a regular one
 	else if (in_board(line + dir[0], column + dir[1]))
 	{
 		if (picked_piece_size(game) == ONE || movement_left(game) == 1)
@@ -345,7 +411,7 @@ bool is_move_possible(board game, direction direction)
 
 void reset_game_move(board game)
 {
-
+	// Reseting piece's related variables to what they were before the move
 	game->grid[picked_piece_line(game)][picked_piece_column(game)] = picked_piece_size(game);
 	game->picked_line = -1;
 	game->picked_column = -1;
@@ -364,6 +430,7 @@ return_code move_piece(board game, direction direction)
 {
 	int *dir = move(direction);
 
+	// Checking all possible errors to throw before moving the piece
 	if (picked_piece_owner(game) == NO_PLAYER)
 	{
 		return EMPTY;
@@ -412,7 +479,9 @@ return_code move_piece(board game, direction direction)
 		if (hovered_size == NONE)
 		{
 			reset_game_move(game);
-		} else {
+		}
+		else
+		{
 			game->movement_left += hovered_size;
 		}
 	}
@@ -422,11 +491,13 @@ return_code move_piece(board game, direction direction)
 
 return_code cancel_movement(board game)
 {
+	// If there is no player that is moving a piece the function will return EMPTY code
 	if (picked_piece_owner(game) == NO_PLAYER)
 	{
 		return EMPTY;
 	}
 
+	// Putting the piece back to its position
 	game->picked_line = game->origin_line;
 	game->picked_column = game->origin_column;
 
@@ -437,7 +508,6 @@ return_code cancel_movement(board game)
 
 return_code cancel_step(board game)
 {
-	// To be continued...
 	if (picked_piece_owner(game) == NO_PLAYER)
 	{
 		return EMPTY;
@@ -445,16 +515,18 @@ return_code cancel_step(board game)
 
 	int last = -4;
 
-	for(int i = 0; i < 3; i++) {
-		if(game->moves[i] == -1) {
+	for (int i = 0; i < 3; i++)
+	{
+		if (game->moves[i] == -1)
+		{
 			last = i - 1;
 			break;
 		}
 	}
 
-	printf("%d", last);
-
-	if(last != -4) {
+	if (last != -4)
+	{
+		// Going one step back
 		direction last_move_direction = game->moves[last];
 		int *dir = move(last_move_direction);
 
@@ -468,6 +540,11 @@ return_code cancel_step(board game)
 
 return_code swap_piece(board game, int target_line, int target_column)
 {
+	if (picked_piece_owner(game) == NO_PLAYER)
+	{
+		return FORBIDDEN;
+	}
+
 	size hovered_size = get_piece_size(game, picked_piece_line(game), picked_piece_column(game));
 
 	if (!in_board(target_line, target_column))
